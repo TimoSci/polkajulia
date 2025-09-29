1db26921ce762d1109ed7c9572705ea0e261b9c7
TimoSci <me@timosci.net>
2025-09-29 17:38:16 -0300

    displaying correlation matrix

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -1,3 +1,6 @@
+using Statistics
+using MultivariateStats 
+
 function create_binary_matrix(df)
 
     binary_matrix_df = unstack(df, :era, :address, :eraPoints)
@@ -9,4 +12,14 @@ end
 
 function varying_validators_addresses(binary_matrix_df)
     return [name for name in names(binary_matrix_df)[2:end] if length(unique(binary_matrix_df[!, name])) > 1]
+end
+
+function correlation_matrix(mat)
+    mat_t = mat' # transpose
+    n_val = size(mat_t, 1)
+    cor_mat = Matrix{Float64}(undef, n_val, n_val)
+    for i in 1:n_val, j in 1:n_val
+        cor_mat[i, j] = cor(mat_t[i, :], mat_t[j, :])
+    end
+    return cor_mat
 end
--- src/create_heatmap.jl
+++ src/create_heatmap.jl
@@ -1,4 +1,6 @@
 using DataFrames
+using StatsPlots  # para heatmap e scatter PCA
+
 include("filters.jl")
 include("load.jl")
 include("algorithms.jl")
@@ -6,14 +8,18 @@ include("algorithms.jl")
 df = load_eras_data()
 df_pruned = remove_missing(df)
 
-binary_matrix = create_binary_matrix(df_pruned)
+binary_matrix_df = create_binary_matrix(df_pruned)
 
 # display(df_pruned)
 # display(df)
 
 # display(binary_matrix)
-varying_addresses = varying_validators_addresses(binary_matrix)
+varying_addresses = varying_validators_addresses(binary_matrix_df)
 
-filtered_matrix = filter_varying(binary_matrix, varying_addresses)
+filtered_matrix_df = filter_varying(binary_matrix_df, varying_addresses)
 
-display(filtered_matrix)
+# println(filtered_matrix_df)
+
+mat = Matrix(filtered_matrix_df[:, 2:end])
+
+println(correlation_matrix(mat))
