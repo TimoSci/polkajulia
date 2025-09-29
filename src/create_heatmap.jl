38477857733e9882b8f98afaf6b6a4afe1582ce3
TimoSci <me@timosci.net>
2025-09-29 17:57:47 -0300

    saving heatmap png

--- src/create_heatmap.jl
+++ src/create_heatmap.jl
@@ -21,5 +21,23 @@ filtered_matrix_df = filter_varying(binary_matrix_df, varying_addresses)
 # println(filtered_matrix_df)
 
 mat = Matrix(filtered_matrix_df[:, 2:end])
+cor_mat = correlation_matrix(mat)
 
-println(correlation_matrix(mat))
+# val_names = names(df)[2:end]
+val_names = varying_addresses #TODO replace with search function for names instead of addresses
+
+hm= heatmap(
+           val_names, val_names, cor_mat;
+           xlabel = "Validador",
+           ylabel = "Validador",
+           title = "Validador Absence Correlation Heatmap",
+           c = :balance,  # diverging colormap
+           clim = (-1, 1), # correlações vão de -1 a 1
+           size = (900, 900),
+           dpi = 300,
+           yflip = true,
+           legend = true
+       )
+
+# display(hm)
+savefig(hm, "heatmap_validadores.png")
