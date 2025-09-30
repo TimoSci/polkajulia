f8bd1b52e4f1727d2218ed03404eeea51d0fb03d
TimoSci <me@timosci.net>
2025-09-30 09:42:43 -0300

    cleanup

--- src/create_heatmap.jl
+++ src/create_heatmap.jl
@@ -10,15 +10,9 @@ df_pruned = remove_missing(df)
 
 binary_matrix_df = create_binary_matrix(df_pruned)
 
-# display(df_pruned)
-# display(df)
-
-# display(binary_matrix)
 varying_addresses = varying_validators_addresses(binary_matrix_df)
 
 filtered_matrix_df = filter_varying(binary_matrix_df, varying_addresses)
-
-# println(filtered_matrix_df)
 
 mat = Matrix(filtered_matrix_df[:, 2:end])
 cor_mat = correlation_matrix(mat)
