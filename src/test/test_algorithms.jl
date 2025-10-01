58148fbb5746a03ce717e1f015bfdf583aa9358b
TimoSci <me@timosci.net>
2025-10-01 20:28:37 -0300

    test more fixtures

--- src/test/test_algorithms.jl
+++ src/test/test_algorithms.jl
@@ -3,7 +3,7 @@ include("../algorithms.jl")
 include("../load.jl")
 
 
-# All Values 1
+# All Values nonmissing
 
 df = CSV.read("./fixtures/testset01.csv", DataFrame)
 display(df)
@@ -20,7 +20,7 @@ display(corr_mat)
 
 
 
-# Random Values
+# Random Values missing
 
 df = CSV.read("./fixtures/testset02.csv", DataFrame)
 display(df)
@@ -34,3 +34,19 @@ corr_mat = correlation_matrix(binary_matrix)
 @test !all(corr_mat .== 1.0)
 display(corr_mat)
 
+
+# Almost all Values nonmissing
+
+df = CSV.read("./fixtures/testset03.csv", DataFrame)
+display(df)
+
+binary_matrix_df = create_binary_matrix(df)
+binary_matrix = Matrix(binary_matrix_df[:, 2:end])
+display(binary_matrix) 
+
+# @test all(binary_matrix .== 1)
+corr_mat = correlation_matrix(binary_matrix)
+display(corr_mat)
+phi_mat = phi_matrix(binary_matrix)
+# @test !all(corr_mat .== 1.0)
+display(phi_mat)
